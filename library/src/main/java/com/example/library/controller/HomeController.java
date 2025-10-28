6252ee1b5425778931a1adb27f83a483e5b71e5d
Mung Ho <mung.hokha@gmail.com>
2025-10-28 23:51:12 +0700

    lending page

--- library/src/main/java/com/example/library/controller/HomeController.java
+++ library/src/main/java/com/example/library/controller/HomeController.java
@@ -7,4 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index() { return "index"; } // templates/index.html
+
+
+    @GetMapping("/admin")
+    public String admin() { return "admin/home/home"; }
 }
